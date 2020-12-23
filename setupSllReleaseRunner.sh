31f17b75e061c0a33261ce3b2667c0df958c6ef2
Piyawasin Pikror-ngarn <piyawasin@simyasolutions.com>
2020-12-23 20:58:03 +0700

    Remove unused Android packages

--- setupSllReleaseRunner.sh
+++ setupSllReleaseRunner.sh
@@ -64,47 +64,15 @@ mkdir --parents "$USER_HOME/.android/"
 echo '### User Sources for Android SDK Manager' >"$USER_HOME/.android/repositories.cfg"
 yes | "$ANDROID_HOME"/tools/bin/sdkmanager --licenses >/dev/null
 yes | "$ANDROID_HOME"/tools/bin/sdkmanager \
-    "platforms;android-16" \
-    "platforms;android-17" \
     "platforms;android-18" \
-    "platforms;android-19" \
-    "platforms;android-20" \
-    "platforms;android-21" \
-    "platforms;android-22" \
-    "platforms;android-23" \
-    "platforms;android-24" \
-    "platforms;android-25" \
-    "platforms;android-26"
+    "platforms;android-28" \
+    "platforms;android-29"
 yes | "$ANDROID_HOME"/tools/bin/sdkmanager "platform-tools"
 yes | "$ANDROID_HOME"/tools/bin/sdkmanager \
-    "build-tools;24.0.0" "build-tools;24.0.1" \
-    "build-tools;24.0.2" "build-tools;24.0.3" \
-    "build-tools;25.0.0" "build-tools;25.0.1" "build-tools;25.0.2" "build-tools;25.0.3" \
-    "build-tools;26.0.0" "build-tools;26.0.1" "build-tools;26.0.2"
-yes | "$ANDROID_HOME"/tools/bin/sdkmanager \
-    "build-tools;17.0.0" \
-    "build-tools;18.1.1" \
-    "build-tools;19.1.0" \
-    "build-tools;20.0.0" \
-    "build-tools;21.1.2" \
-    "build-tools;22.0.1" \
-    "build-tools;23.0.1" "build-tools;23.0.2" "build-tools;23.0.3"
+    "build-tools;28.0.3"
 yes | "$ANDROID_HOME"/tools/bin/sdkmanager \
     "extras;android;m2repository" \
     "extras;google;m2repository"
-yes | "$ANDROID_HOME"/tools/bin/sdkmanager \
-    "extras;google;google_play_services" \
-    "extras;m2repository;com;android;support;constraint;constraint-layout;1.0.1" \
-    "extras;m2repository;com;android;support;constraint;constraint-layout;1.0.2"
-yes | "$ANDROID_HOME"/tools/bin/sdkmanager \
-    "add-ons;addon-google_apis-google-16" \
-    "add-ons;addon-google_apis-google-17" \
-    "add-ons;addon-google_apis-google-18" \
-    "add-ons;addon-google_apis-google-19" \
-    "add-ons;addon-google_apis-google-21" \
-    "add-ons;addon-google_apis-google-22" \
-    "add-ons;addon-google_apis-google-23" \
-    "add-ons;addon-google_apis-google-24"
 
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh &&
     apt-get update && apt-get install git-lfs
